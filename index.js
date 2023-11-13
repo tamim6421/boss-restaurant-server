86b21ea777aea297e57d0e87e29a8e26d88a86d5
ProgrammingHero <tamimhossain6421@gmail.com>
2023-11-14 01:12:37 +0600

    add cart data and delete cart data

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.PROJECT_NAME}:${process.env.PROJECT_PASS}@cluster0.iimwc2a.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -24,7 +24,10 @@ const client = new MongoClient(uri, {
 
 const menuCollection = client.db("bossDb").collection('menu')
 const reviewCollection = client.db("bossDb").collection('reviews')
+const cartCollection = client.db("bossDb").collection('carts')
 
+
+// get all menus 
 app.get('/menu', async(req, res) =>{
   try {
     const query = menuCollection.find()
@@ -35,10 +38,49 @@ app.get('/menu', async(req, res) =>{
   }
 })
 
+
+// get all review
 app.get('/review', async(req, res) =>{
   try {
     const query = reviewCollection.find()
     const result = await query.toArray()
+    res.send(result)
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+
+// create cart collection 
+app.post('/carts', async(req, res) =>{
+  try {
+    const cartItem = req.body 
+    const result = await cartCollection.insertOne(cartItem)
+    res.send(result)
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+// get all cart collection 
+app.get('/carts', async(req, res) =>{
+  try {
+    const email = req.query.email 
+    const query = {email: email}
+    console.log(email)
+    const result = await cartCollection.find(query).toArray()
+    res.send(result)
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+// delete cart 
+app.delete('/carts/:id', async(req, res) =>{
+  try {
+    const id = req.params.id 
+    const query = {_id: new ObjectId(id)}
+    const result = await cartCollection.deleteOne(query)
     res.send(result)
   } catch (error) {
     console.log(error)
