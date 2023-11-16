25144800840163852964a4a1fe471e8a8d7095e2
ProgrammingHero <tamimhossain6421@gmail.com>
2023-11-16 16:29:09 +0600

    implement jwt and verified middleware

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
 const app = express()
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000 
 
 // middleware 
@@ -22,9 +23,125 @@ const client = new MongoClient(uri, {
   }
 });
 
+const usersCollection = client.db("bossDb").collection('users')
 const menuCollection = client.db("bossDb").collection('menu')
 const reviewCollection = client.db("bossDb").collection('reviews')
 const cartCollection = client.db("bossDb").collection('carts')
+
+
+
+// jwt related api 
+app.post('/jwt', async(req, res) =>{
+  const user = req.body 
+  const token = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: '1hr'})
+  res.send({token})
+})
+
+
+// users related api 
+
+// middleware
+const verifyToken = (req, res, next)=>{
+  console.log('inside verify token', req.headers.authorization)
+ 
+  if(!req.headers.authorization){
+    return res.status(401).send({message: 'unauthorized access'})
+  }
+  const token = req.headers.authorization.split(' ')[1]
+  jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) =>{
+    if(err){
+      return res.status(401).send({message: 'unauthorized access'})
+    }
+    req.decoded = decoded;
+    next()
+  })
+ 
+}
+
+
+//use verify admin after verify token 
+const verifyAdmin = async(req, res, next) =>{
+  const email = req.decoded.email 
+  const query = {email: email}
+  const user = await usersCollection.findOne(query)
+  const isAdmin = user?.role === 'admin'
+  if(!isAdmin){
+    return res.status(403).send({message: 'forbidden access'})
+  }
+  next()
+}
+
+// get total users for admin 
+app.get('/users', verifyToken,verifyAdmin, async(req, res) =>{
+    // console.log(req.headers)
+    const result = await usersCollection.find().toArray()
+    res.send(result)
+})
+
+// delete users 
+app.delete('/users/:id', verifyToken, verifyAdmin, async(req, res) =>{
+  try {
+    const id = req.params.id 
+  const query = {_id: new ObjectId(id)}
+  const result = await usersCollection.deleteOne(query)
+  res.send(result)
+    
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+//check admin role get email 
+app.get('/users/admin/:email', verifyToken, async(req, res) =>{
+  const email = req.params.email 
+
+  if(email !== req.decoded.email){
+    return res.status(403).send({message:'forbidden access'})
+  }
+  const query = {email: email}
+  const user = await usersCollection.findOne(query)
+  let admin = false 
+  if(user){
+    admin = user?.role === 'admin'
+  }
+  res.send({admin})
+})
+
+// edit users roll 
+app.patch('/users/admin/:id', verifyToken, verifyAdmin, async(req, res) =>{
+  try {
+    const id = req.params.id 
+    const filter = {_id: new ObjectId(id)}
+    const updatedDoc = {
+      $set:{
+        role: 'admin'
+      }
+    }
+    const result = await usersCollection.updateOne( filter, updatedDoc)
+    res.send(result)
+    
+  } catch (error) {
+    console.log(error)
+  }
+
+})
+
+// post users info 
+app.post('/users', async(req, res) =>{
+  const user = req.body 
+
+
+
+  // insert email if user don not exists 
+  // simple checking 
+  const query = {email: user.email}
+  const existingUser = await usersCollection.findOne(query)
+  if(existingUser){
+    return res.send({message: 'user already exist', insertedId: null})
+  }
+  const result = await usersCollection.insertOne(user)
+  res.send(result)
+})
 
 
 // get all menus 
